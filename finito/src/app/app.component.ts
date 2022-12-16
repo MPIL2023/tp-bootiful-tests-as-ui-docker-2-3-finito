fc3c02d168658cf76061d8eb370228ea3dbb9c22
Alpaslan <gebesoglu.alpaslan@gmail.com>
2022-12-16 15:27:14 +0100

    get current

--- finito/src/app/app.component.ts
+++ finito/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+currentInt : number;
+getCurrent() {
+  var a = this._http.get<number>("http://localhost:8080/adder/current").subscribe(value => {
+      this.currentInt=value;
+  });
+}
+
+constructor(private _http: HttpClient){
+  this.currentInt=-1;
+}
   title = 'finito';
 }
