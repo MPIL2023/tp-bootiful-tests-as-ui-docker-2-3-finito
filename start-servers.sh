24a6f97f2d84189509f7fca21c18de6292e44c8e
Elio <elliotfaugier@gmail.com>
2023-03-02 16:15:53 +0100

    Update start-servers.sh

--- start-servers.sh
+++ start-servers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo
+echo "AHAHAHA"
 cd /home/work/tp-bootiful-tests-as-ui-docker-2-3-finito/finito && yarn ng serve --host 0.0.0.0 &
 cp /home/work/tp-bootifultests---rest-api-1-3-finito/finitoApp/target/finitoApp-0.0.1-SNAPSHOT.jar /home/work/finitoApp-0.0.1-SNAPSHOT.jar
 java -jar /home/work/finitoApp-0.0.1-SNAPSHOT.jar
